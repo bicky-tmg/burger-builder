eaad81b01770fc5d9f732ae95107bd5a4dae3b79
Bicky Tamang <bickytamang101@gmail.com>
2020-08-24 17:21:16 +0545

    burger side

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Aux from '../../hoc/Auxilliary';
+import classes from './Layout.css';
+
+const layout = (props) => ( 
+    <Aux>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aux>
+);
+
+ 
+export default layout;
