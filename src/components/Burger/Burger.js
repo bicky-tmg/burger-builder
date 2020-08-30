d34be938838f81c3af3329d60f06cf2e5781427c
Bicky Tamang <bickytamang101@gmail.com>
2020-08-30 21:08:35 +0545

    Adding routing to the project

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,6 +5,7 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
+    console.log(props);
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
